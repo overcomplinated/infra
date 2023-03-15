8f222c1af15326a1d0bf4a5d9f885e261116653b
unknown <Administrator@ONPREM.INTERNAL>
2023-03-15 12:00:59 -0700

    start

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
-resource "google_storage_bucket" "tfState" {
-  name          = "tfState"
+resource "google_storage_bucket" "overcomplinated-tfState-1985" {
+  name          = "overcomplinated-tfState-1985"
   location      = "US"
   storage_class = "STANDARD"
 }
